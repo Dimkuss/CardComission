07440cc413ef77634e8b2c31ec4cf7ec1b27cbbc
Mokhov Dmitriy <mohow-95@mail.ru>
2021-05-15 16:48:18 +0300

    fix fix

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,23 @@
-fun main(){
-fun calculate(cardType : String,allSum: Int,sum:Int){
-    var comission=0.0
-    when (cardType){
-        "Maestro","Mastercard" -> {
-            if (sum in 0..75000){
+fun main() {
+
+    calculate("Mastercard", 0, 100000)
+}
+
+fun calculate(cardType: String, allSum: Int, sum: Int) {
+    var comission = 0.0
+    when (cardType) {
+        "Maestro", "Mastercard" -> {
+            if (sum in 0..75000) {
                 println("Ваша комиссия 0 рублей")
             } else {
-                comission =sum*0.006*100+2000
+                comission = sum * 0.006 * 100 + 2000
                 println("Ваша комиссия $comission копеек")
             }
         }
-        "Visa","Мир" -> {
-            comission=sum*0.0075
+        "Visa", "Мир" -> {
+            comission = sum * 0.0075
 
-            if (comission<3500){
+            if (comission < 3500) {
                 println("Комиссия будет 35 рублей")
             } else {
                 println("Комиссия будет $comission копеек")
@@ -23,10 +27,4 @@ fun calculate(cardType : String,allSum: Int,sum:Int){
             println("У вас нет комиссии, поздравляем")
         }
     }
-}
-calculate("Mastercard",0,100000)
-
-
-
-
 }
